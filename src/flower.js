12afecba9c2b579ec217c71a866f09a6e3a2e7e2
Kevin Cox <kev.r.cox90@gmail.com>
2015-07-18 18:31:28 -0700

    initial rocket experiment setup

--- src/flower.js
+++ src/flower.js
@@ -1,13 +1,9 @@
 window.globals = window.globals || {};
 
 //settings variables
-var numSegments = 10;
-var rotation = 360/numSegments;
 var centerX = $('canvas').width()/2;
 var centerY = $('canvas').height()/2;
 var centerPoint = new Point(centerX, centerY);
-var segLength = Math.min($('canvas').width(), $('canvas').height()) * 0.48;
-var guide;
 
 //setup global properties
 globals.centerPoint = centerPoint;
@@ -17,202 +13,66 @@ globals.brushType = 'auto';
 globals.animationQueue = [];
 globals.$activeLayer = undefined;
 
+//define tool
+var drawTool = new Tool();
+window.globals.drawTool = drawTool;
 
-//create inital layers
-var guideLayer = new Layer();
-addWorkingLayer();
-updateSegmentDisplay();
-drawGuide();
-globals.drawTool.showSettingsBox();
+//tool settings
+var sizingType = 'auto';
+var brushSize = 5;
+var colors = ["#A2BF68","#569E5F","#CC746F","#A45983"];
+var drawPath;
+var drawThicknessScale = 0.025;
+var minLineSize = 0.75;
 
-//set button events
-$('button.rotate').on('click', drawRotation);
-$('button.clear').on('click', clearDrawing);
-$('button.add-segment').on('click', addSegment);
-$('button.remove-segment').on('click', removeSegment);
-$('button.tool').on('click', selectTool);
-$('button.add-layer').on('click', addWorkingLayer);
-
-function drawGuide() {
-  currLayer = project.activeLayer;
-  guideLayer.activate();
-
-  if (guide) {
-    guide.remove();
-  }
-
-  var t1 = centerPoint;
-  var t2 = new Point(centerPoint.x, centerPoint.y - segLength);
-  t2 = t2.rotate(rotation/2, centerPoint);
-  var t3 = new Point(centerPoint.x, centerPoint.y - segLength);
-  t3 = t3.rotate(360 - rotation/2, centerPoint);
-
-  guide = new Path();
-  guide.add(t1);
-  guide.add(t2);
-  guide.add(t3);
-  guide.closed = true;
-  guide.strokeColor = "#4D4D4D";
-
-  paper.view.draw();
-  currLayer.activate();
-}
-
-function drawRotation() {
-  var rotationQueue = [];
-  project.activeLayer.children.forEach(function(child) {
-    if (!child.rotated) {
-      rotationQueue.push(child);
-    }
-  });
-
-  rotationQueue.forEach(function(child) {
-    _rotate(child);
-  });
-}
-
-function _rotate(shape) {
-  var shapeCopy = rotationCopy(shape);
-  for (i = 0; i < numSegments - 1; i++) {
-    shapeCopy.rotate(rotation, centerPoint);
-    shapeCopy = rotationCopy(shapeCopy);
-  }
-  shape.rotated = true;
-}
-
-function rotationCopy(shape) {
-  var newShape = shape.clone();
-  newShape.rotated = true;
-  return newShape;
-}
-
-function onFrame(event) {
-  globals.layers.forEach(function(layer) {
-    if (layer.animation) {
-      layer.animation(layer, layer.animSpeed, layer.animOptions);
-    }
-  });
-}
-
-function clearDrawing() {
-  removeQueue = [];
-  project.activeLayer.children.forEach(function(child) {
-    removeQueue.push(child);
-  });
-
-  removeQueue.forEach(function(child) {
-    child.remove();
-  });
-}
-
-function addSegment() {
-  numSegments += 1;
-  rotation = 360/numSegments;
-  updateSegmentDisplay();
-  drawGuide();
-}
-
-function removeSegment() {
-  if (numSegments > 3) {
-    numSegments -= 1;
-    rotation = 360/numSegments;
-    updateSegmentDisplay();
-    drawGuide();
-  }
-}
-
-function updateSegmentDisplay() {
-  $('h4.segment-counter').html('Segments: ' + numSegments);
-}
-
-function selectTool(event) {
-  // console.log($(event.target));
-  if ($(event.target).hasClass('circle-tool')) {
-    globals.shapeTool.activate();
-    globals.shapeTool.showSettingsBox();
-  }
-  else if ($(event.target).hasClass('draw-tool')) {
-    globals.drawTool.activate();
-    globals.drawTool.showSettingsBox();
-  }
-}
-
-function addWorkingLayer() {
-  if (globals.$activeLayer) {
-    globals.$activeLayer.removeClass('active-layer');
-  }
-
-  newLayer = new Layer();
-  newLayer.animation = undefined;
-
-  globals.layers = globals.layers || [];
-  globals.layers.push(newLayer);
-
-  $layerDeleteButton = $('<button>').addClass('delete-layer');
-  $layerDeleteButton.html('-');
-  $layerDeleteButton.on('click', deleteLayer);
-
-  $layerRotateButton = $('<button>').addClass('rotate-anim');
-  $layerRotateButton.html('Rotate');
-  $layerRotateButton.on('click', globals.animation.rotateLayer);
-
-  $layerBounceButton = $('<button>').addClass('bounce-anim');
-  $layerBounceButton.html('Bounce');
-  $layerBounceButton.on('click', globals.animation.bounceLayer);
-
-  $layerUiItem = $('<li>').html('Layer ' + newLayer.id);
-  $layerUiItem.append($layerDeleteButton);
-  $layerUiItem.append($layerRotateButton);
-  $layerUiItem.append($layerBounceButton);
-  $layerUiItem.attr('id', newLayer.id);
-
-  $('.layers ul').append($layerUiItem);
-
-  $layerUiItem.on('click', setActiveLayer);
-  $layerUiItem.addClass('active-layer');
-  globals.$activeLayer = $layerUiItem;
-}
-
-function setActiveLayer(event) {
-  $layerUiItem = $(event.delegateTarget);
-
-  globals.$activeLayer.removeClass('active-layer');
-  $layerUiItem.addClass('active-layer');
-  globals.$activeLayer = $layerUiItem;
-
-  targetLayerId = parseInt($layerUiItem.attr('id'));
-  targetLayer = globals.findLayerById(targetLayerId);
-  targetLayer.activate();
-  console.log('setActiveLayer clicked!');
-}
-
-function deleteLayer(event) {
-  //only delete if there is more than 1 layer - cannot have no layers in a project
-  if (globals.layers.length > 1) {
-
-    $layerUiItem = $(event.target).parent();
-    targetLayerId = parseInt($layerUiItem.attr('id'));
-
-    //remove layer from global list
-    for (var i = 0; i < globals.layers.length; i++) {
-      if (globals.layers[i].id === targetLayerId) {
-        globals.layers.splice(i, 1);
-      }
-    }
-
-    globals.findLayerById(targetLayerId).remove();
-    $layerUiItem.remove();
-
-    paper.view.draw();
-  }
-}
-
-globals.findLayerById = function (targetId) {
-  targetLayer = undefined;
-  project.layers.forEach(function(layer) {
-    if (layer.id === targetId) {
-      targetLayer = layer;
-    }
-  });
-  return targetLayer;
+drawTool.onMouseDown = function (event) {
+  drawPath = new Path();
+  drawPath.rotated = false;
+  project.activeLayer.addChild(drawPath);
+  drawPath.fillColor = colors[Math.floor(Math.random() * colors.length)];
 };
+
+drawTool.onMouseDrag = function (event) {
+  var step = event.delta;
+
+  if (sizingType === 'auto') {
+    distVector = globals.centerPoint - event.point;
+    distFromCenter = distVector.length - 50;
+    if (distFromCenter < minLineSize / drawThicknessScale) {
+      distFromCenter = minLineSize / drawThicknessScale;
+    }
+
+    step.angle += 90;
+    step.length = distFromCenter * drawThicknessScale;
+  }
+
+  if (sizingType === 'manual') {
+    step.angle += 90;
+    step.length = brushSize;
+  }
+
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
+
+  drawPath.add(top);
+  drawPath.insert(0, bottom);
+  drawPath.smooth();
+};
+
+drawTool.onMouseUp = function (event) {
+  drawPath.add(event.point);
+	drawPath.closed = true;
+	drawPath.smooth();
+};
+
+setTimeout(function () {
+  breakit();
+}, 3000);
+
+// function onFrame(event) {
+//   globals.layers.forEach(function(layer) {
+//     if (layer.animation) {
+//       layer.animation(layer, layer.animSpeed, layer.animOptions);
+//     }
+//   });
+// }
--- src/tools/draw_tool.js
+++ src/tools/draw_tool.js
@@ -20,6 +20,8 @@ drawTool.onMouseDown = function (event) {
 };
 
 drawTool.onMouseDrag = function (event) {
+  var step = event.delta;
+
   if (sizingType === 'auto') {
     distVector = globals.centerPoint - event.point;
     distFromCenter = distVector.length - 50;
@@ -27,13 +29,11 @@ drawTool.onMouseDrag = function (event) {
       distFromCenter = minLineSize / drawThicknessScale;
     }
 
-    var step = event.delta;
     step.angle += 90;
     step.length = distFromCenter * drawThicknessScale;
   }
 
   if (sizingType === 'manual') {
-    var step = event.delta;
     step.angle += 90;
     step.length = brushSize;
   }
@@ -50,65 +50,4 @@ drawTool.onMouseUp = function (event) {
   drawPath.add(event.point);
 	drawPath.closed = true;
 	drawPath.smooth();
-};
-
-drawTool.showSettingsBox = function () {
-  var $settingsBox = $('<div>');
-  $settingsBox.addClass('draw-settings');
-  $settingsBox.html('Draw Settings');
-
-  $autoSizeArea = $('<div>');
-  $autoSizeArea.addClass('drawing-size-selector');
-  $autoSizeArea.addClass('auto-size');
-  $autoSizeArea.addClass('active-size');
-  $autoSizeArea.on('click', drawTool.setAutoSize);
-  $autoSizeArea.html("Auto");
-  $settingsBox.append($autoSizeArea);
-
-  $manualSizeArea = $('<div>');
-  $manualSizeArea.addClass('drawing-size-selector');
-  $manualSizeArea.addClass('manual-size');
-  $manualSizeArea.on('click', drawTool.setManualSize);
-  $settingsBox.append($manualSizeArea);
-
-  $manualBrushDemo = $('<div>');
-  $manualBrushDemo.addClass('brush-size-demo');
-  $manualBrushDemo.css('width', brushSize + 4);
-  $manualBrushDemo.css('height', brushSize + 4  );
-  $manualSizeArea.append($manualBrushDemo);
-
-  $manualSizeUp = $('<button>');
-  $manualSizeUp.html('+');
-  $manualSizeUp.on('click', function () {
-    brushSize += 2;
-    $manualBrushDemo.css('width', brushSize + 4);
-    $manualBrushDemo.css('height', brushSize + 4);
-  }.bind(this));
-  $manualSizeArea.append($manualSizeUp);
-
-  $manualSizeDown = $('<button>');
-  $manualSizeDown.html('-');
-  $manualSizeDown.on('click', function () {
-    if (brushSize > 1) {
-      brushSize -= 2;
-      $manualBrushDemo.css('width', brushSize + 4);
-      $manualBrushDemo.css('height', brushSize + 4);
-    }
-  }.bind(this));
-  $manualSizeArea.append($manualSizeDown);
-
-  $('.settings').html('');
-  $('.settings').append($settingsBox);
-};
-
-drawTool.setAutoSize = function (event) {
-  sizingType = 'auto';
-  $('.active-size').removeClass('active-size');
-  $(event.delegateTarget).addClass('active-size');
-};
-
-drawTool.setManualSize = function (event) {
-  sizingType = 'manual';
-  $('.active-size').removeClass('active-size');
-  $(event.delegateTarget).addClass('active-size');
 };
